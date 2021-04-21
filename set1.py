ea832cc50a523e5a1a7b64a326627468f7da4a81
iftelhar <x@gmail.com>
2021-04-22 01:38:19 +0600

    comment added

--- set1.py
+++ set1.py
@@ -1,3 +1,13 @@
+'''
+find the average of set values
+
+this problem take the list make it set using set()
+
+then we calculate the sum of set value and divided by len of set value,
+
+round up 3 digit and return the value
+
+'''
 def average(array):
     # your code goes here
     set_value = set(array)
